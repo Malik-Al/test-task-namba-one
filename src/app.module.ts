e68c8e3ed55ebd08ccc924a6c834ebc825d405be
malik <malik@localhost.localdomain>
2023-03-04 21:22:44 +0600

    Connected to posgtres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entity/product.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env'
    }),
+   TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRESS_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRESS_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    entities: [Product],
+    synchronize: true,
+  }),
+    ProductModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
