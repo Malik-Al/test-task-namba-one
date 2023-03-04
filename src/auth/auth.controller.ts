9a1cb181de02eb9e9ff5a8d45814108bc4bebf2c
malik <malik@localhost.localdomain>
2023-03-05 03:47:06 +0600

    Added pipe and validator for registration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import { ValidationPipe } from 'src/pipes/validation.pipe';
 import { CreateUserDto } from 'src/user/dto/create.user.dto';
 import { AuthService } from './auth.service';
+
 
 @Controller('auth')
 export class AuthController {
@@ -10,7 +12,7 @@ export class AuthController {
     login(@Body() userDto: CreateUserDto){
         return this.authService.login(userDto)
     }
-
+    @UsePipes(ValidationPipe)
     @Post('/registration')
     registration(@Body() userDto: CreateUserDto){
         return this.authService.registration(userDto)
--- src/user/dto/create.user.dto.ts
+++ src/user/dto/create.user.dto.ts
@@ -1,4 +1,9 @@
+import { IsString, Length } from "class-validator";
+
 export class CreateUserDto {
+    @IsString({message: 'Должна быть строкой'})
     readonly login: string;
+
+    @Length(6, 16,{message: 'Не меньше 6 и не больше 16'})
     readonly password: string;
 }
