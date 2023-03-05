709ab28020d40fd34e05e99eb6a1d68e9ce26663
malik <malik@localhost.localdomain>
2023-03-05 21:01:15 +0600

    Added dto update.product.do.ts to swagger

--- src/product/dto/update.product.dto.ts
+++ src/product/dto/update.product.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class UpdateProductDto {
+    @ApiProperty({example: 'laptop', description: 'Название товара'})  
     readonly name: string;
+
+    @ApiProperty({example: '80 236', description: 'Цена товара'})  
     readonly price: number;
+
+    @ApiProperty({example: 'true', description: 'Статус наличие товара'})  
     readonly isActive: boolean;
 }
